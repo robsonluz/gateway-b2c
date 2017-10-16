0579b778f2f9b89233f6fee9234583ddb3e6fccd
Robson Luz <robson.luz@gmail.com>
2017-10-16 16:33:56 -0200

    Metodos delete

--- src/main/java/edu/usf/produto/controller/PedidoController.java
+++ src/main/java/edu/usf/produto/controller/PedidoController.java
@@ -5,7 +5,10 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -30,5 +33,15 @@ public class PedidoController {
 
 		HttpEntity<Map<String, Object>> request = new HttpEntity<>(produto, headers);
 		return restTemplate.postForEntity("http://service-pedido:8080/pedidos", request, Object.class);
+	}
+	
+	@RequestMapping(path="/{id}", method=RequestMethod.DELETE)
+	public HttpEntity<?> delete(@PathVariable String id) {
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_JSON);
+
+		HttpEntity<Map<String, Object>> request = new HttpEntity<>(headers);
+		restTemplate.delete("http://service-pedido:8080/pedidos/" + id, request);
+		return new ResponseEntity<>(HttpStatus.OK);
 	}
 }
--- src/main/java/edu/usf/produto/controller/ProdutoController.java
+++ src/main/java/edu/usf/produto/controller/ProdutoController.java
@@ -5,7 +5,10 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -31,4 +34,14 @@ public class ProdutoController {
 		HttpEntity<Map<String, Object>> request = new HttpEntity<>(produto, headers);
 		return restTemplate.postForEntity("http://service-produto:8080/produtos", request, Object.class);
 	}
+	
+	@RequestMapping(path="/{id}", method=RequestMethod.DELETE)
+	public HttpEntity<?> delete(@PathVariable Long id) {
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_JSON);
+
+		HttpEntity<Map<String, Object>> request = new HttpEntity<>(headers);
+		restTemplate.delete("http://service-produto:8080/produtos/" + id, request);
+		return new ResponseEntity<>(HttpStatus.OK);
+	}	
 }
