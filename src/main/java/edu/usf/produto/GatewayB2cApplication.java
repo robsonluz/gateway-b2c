b61d1b034906fd5032b52c56a95c9dd228145698
Robson Luz <robson.luz@gmail.com>
2017-10-16 17:28:05 -0200

    Cors

--- src/main/java/edu/usf/produto/GatewayB2cApplication.java
+++ src/main/java/edu/usf/produto/GatewayB2cApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class GatewayB2cApplication {
@@ -12,6 +15,16 @@ public class GatewayB2cApplication {
 	public RestTemplate restTemplate() {
 		return new RestTemplate();
 	}
+	
+	@Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("*").allowedOrigins("*");
+            }
+        };
+    }	
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayB2cApplication.class, args);
